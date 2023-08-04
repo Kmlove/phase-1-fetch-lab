6af8827bba2a6a6882d870a20c8e387822dd5d47
Kim Love <kimberlymlove15@gmail.com>
2023-08-04 18:19:42 -0400

    Adds js to index.js. Acheived deliverable but tests don't pass

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+            .then((res) => res.json());
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+//The Promise containing the js code
+let returnedJSON = fetchBooks();
+
+returnedJSON.then(data => renderBooks(data))
+// returnedJSON.then(function(data){
+//   renderBooks(data)
+// })
+
+// DOESN'T PASS TEST BUT RENDERS THE BOOK TITLES TO THE DOM
